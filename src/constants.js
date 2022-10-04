35e8ec2190b4cd7b2a2aef79ecfd3a1286b4dd81
javabipe <67403018+javabipe@users.noreply.github.com>
2022-10-03 21:28:39 -0300

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+// constants.js
+'use strict';
+
+let constants = {
+    FB_AUTH_DOMAIN: "algar-1616501032135.firebaseapp.com",
+    FB_PROJECT_ID: "algar-1616501032135",
+    FB_MESSAGING_SENDER_ID: "759630716122",
+    FB_APP_IP: "https://algar-1616501032135-default-rtdb.firebaseio.com",
+    FB_STORAGE_BUCKET: "algar-1616501032135.appspot.com",
+    FB_API_KEY: "AIzaSyCg2Rl7gO_CASG-0Ml6rjsRcAWkPoII6s8",
+
+    
+};
+
+module.exports =
+        Object.freeze(constants); // freeze prevents changes by users
